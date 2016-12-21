7bf18aaeb527ad949d44c34aaef0bd1969301887
njausteve <njaustevedomino@gmail.com>
2016-12-21 21:25:59 +0530

    Added ionic Auth (post issue persists)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
-angular.module('app.controllers', ['ionic.cloud'])
+angular.module('app.controllers', ['ionic.cloud', 'ionic.cloud.init'])
 
 .controller('cameraTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
+// You can include an y angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
@@ -41,70 +41,65 @@ function ($scope, $stateParams, $state, $ionicSideMenuDelegate) {
    console.log("inside loginCtrl");
 }])
 
-.controller('signupCtrl', ['$scope', '$state', '$stateParams', '$ionicSideMenuDelegate',  '$ionicAuth', '$ionicUser', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('signupCtrl', ['$scope','$stateParams', '$ionicSideMenuDelegate',  '$ionicUser', '$ionicAuth', '$state', '$q',  // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope,$stateParams, $ionicAuth, $ionicUser,$state ) {
+function ($scope, $stateParams, $ionicSideMenuDelegate, $ionicUser, $ionicAuth, $state, $q) {
+    
+    
+    $scope.data = {
+        'app_id':'8f2e06a0',
+        'name': '',
+        'email': '',
+        'password': ''
+
+    }
+    
+    $scope.error='';
+         
+
+        $scope.signup = function(){
 
 
 
-console.log("inside signupCtrl");
-  //to disableSideMenuDrag
-  //  $ionicSideMenuDelegate.canDragContent(false)
-
-  $scope.signup = signup;
-
-   $scope.data = {   'username': '',
-                     'email': '',
-                     'password': ''
-                  }
-   $scope.error = '';
 
 
 
-
-   function signup(){
-       console.log($ionicAuth);
-
-     var details = {'email': 'bi@ionic.io', 'password': 'puppies123'};
-
-  $ionicAuth.signup(details).then(function() {
-  // `$ionicUser` is now registered
-  }, function(err) {
-  for (var e of err.details) {
-  if (e === 'conflict_email') {
-  alert('Email already exists.');
-  } else {
-  // handle other errors
-  }
-  }
-  });
-      // $scope.error = '';
-      //
-      //  $ionicAuth.signup($scope.data).then(function(){
-      //     // ionicUser is now registered
-      //    $ionicAuth.login('basic', $scope.data).then(function(){
-      //        $state.go('tabsController.cameraTabDefaultPage');
-      //    });
-      //
-      //  }, function(err){
-      //
-      //       var error_lookup = {
-      //                 'required_email': 'missing email field',
-      //                 'required_password': 'missing password field',
-      //                 'conflict_email':'A user has already signed up with that email',
-      //                 'conflict_username':' A user has already signed up with that username',
-      //                 'invalid_email' : 'the email did not pass validation'
-      //               }
-      //
-      //       $scope.error = error_lookup[err.details[0]]
-      //       console.log(err)
-      //  });
-      //
-
-   }
-
-
-
+            var details = {
+'email': '$scope.data.email',
+'password': $scope.data.password
+}
+$ionicAuth.signup(details).then(
+function(user){
+user.migrate();
+user.save(); // save the user to persist the migration changes
+console.log('success');
+},
+function(user){
+console.log(user + 'not added to ionic')
+}
+); 
+        // $scope.error = '';
+        // console.log($scope.data);
+        
+        // $ionicAuth.signup($scope.data).then(function(worked) {
+        //     // `$ionicUser` is now registered
+        //     console.log(worked);
+        //     $ionicAuth.login('basic', $scope.data).then(function(){
+        //       $state.go('menu.home');
+        //     });
+        // }, function(err) {
+            
+        //     var error_lookup = {
+        //         'required_email': 'Missing email field',
+        //         'required_password': 'Missing password field',
+        //         'conflict_email': 'A user has already signed up with that email',
+        //         'conflict_username': 'A user has already signed up with that username',
+        //         'invalid_email': 'The email did not pass validation'
+        //     }    
+        
+        //     $scope.error = error_lookup[err.details[0]];
+        // });
+    }
 
 }])
--- www/js/ionic.cloud.init.js
+++ www/js/ionic.cloud.init.js
@@ -2,7 +2,8 @@ angular.module('ionic.cloud.init', ['ionic.cloud'])
 .config(function($ionicCloudProvider){
       $ionicCloudProvider.init({
         "core" : {
-                   "app_id ": "8f2e06a0"
+                   "app_id ": "8f2e06a0",
+                   "api_key":"f25e465e3c9a81c0daa1a70b47d040ef664ebd1dd04d2a3b"
                  }
       });
 
